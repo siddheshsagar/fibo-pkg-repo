cb0ccfdc3357e0f13b3648868b9bb64ae8e008a3
siddhesh <siddheshsagar3182001@gmail.com>
2022-09-14 10:28:40 +0530

    changed links - SSH to HTTPS, changed remote origin to https link insted of SSH in order to avoid the access issues

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cal_fibonacci',
     version='0.0.1',
     description='A very basic fibonacci calculator',
-    url='git@github.com:siddheshsagar/fibo-pkg-repo.git',
+    url='https://github.com/siddheshsagar/fibo-pkg-repo.git',
     author='Siddhesh Sagar',
     author_email='siddheshsagar3182001@gmail.com',
     license='MIT',
