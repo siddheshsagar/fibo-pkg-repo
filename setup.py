b3daaef86daf5266a5efaf65b7a0ada2932a4d80
siddhesh <siddheshsagar3182001@gmail.com>
2022-09-14 10:21:00 +0530

    added initial files to repo

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cal_fibonacci',
+    version='0.0.1',
+    description='A very basic fibonacci calculator',
+    url='git@github.com:siddheshsagar/fibo-pkg-repo.git',
+    author='Siddhesh Sagar',
+    author_email='siddheshsagar3182001@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False
+)
