327dd5c3071c2ef90e93303144f3d1f699f2ddc9
Siddhesh Sagar <77565933+siddheshsagar@users.noreply.github.com>
2022-09-15 16:22:20 +0530

    Update __init__.py

--- calculate_fibonacci/__init__.py
+++ calculate_fibonacci/__init__.py
@@ -1,11 +1,2 @@
-def calculate_fibonacci(n):
-    if n == 0:
-        return 0
-    if n == 1 or n == 2:
-        return 1
-    a,b = 1,1
-    for i in range(2,n):
-        temp = a+b
-        a = b
-        b = temp
-    return temp
+def hello_world():
+    print("Hello World")
