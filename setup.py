46d1479cc9760b3908b28e595d4b4783e51d710c
Siddhesh Sagar <77565933+siddheshsagar@users.noreply.github.com>
2022-09-15 16:36:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
 from setuptools import setup, find_packages
 
-extra_math = [
-    'returns-decorator',
-]
-
-extra_bin = [
-    *extra_math,
-]
-
-extra_test = [
-    *extra_math,
-    'pytest>=4',
-    'pytest-cov>=2',
-]
-    
-extra_dev = [
-    *extra_test,
-]
-
-extra_ci = [
-    *extra_test,
-    'python-coveralls',
-]
-
 setup(
     name='calculate_fibonacci',
     version='0.0.1',
@@ -31,29 +8,6 @@ setup(
     author='Siddhesh Sagar',
     author_email='siddheshsagar3182001@gmail.com',
     license='MIT',
-    packages=find_packages(),
-    extras_require={
-        'math': extra_math,
-
-        'bin': extra_bin,
-
-        'test': extra_test,
-        'dev': extra_dev,
-
-        'ci': extra_ci,
-    },
-    
-    entry_points={
-        'console_scripts': [
-            'add=calculate_fibonacci.math:calculate_fibonacci',
-        ],
-    },
-    
-    classifiers=[
-        'Intended Audience :: Developers',
-
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-    ],
+    packages=find_packages()
 )
 
